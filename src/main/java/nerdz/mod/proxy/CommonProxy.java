fa2dc93d27437d32b1f76a9e56e3f7b6e63c0f98
arne1001 <arne.vz@hotmail.be>
2018-08-03 11:35:24 +0200

    [mod setup]

---
+++ src/main/java/nerdz/mod/proxy/CommonProxy.java
@@ -0,0 +1,4 @@
+package nerdz.mod.proxy;
+
+public class CommonProxy {
+}
---
+++ src/main/java/nerdz/mod/util/Reference.java
@@ -0,0 +1,10 @@
+package nerdz.mod.util;
+
+public class Reference
+{
+    public static final String MODID = "bm";
+    public static final String NAME = "The Beer Mod";
+    public static final String VERSION = "0.1";
+    public static final String CLIENT = "nerdz.mod.proxy.ClientProxy";
+    public static final String COMMON = "nerdz.mod.proxy.CommonProxy";
+}
